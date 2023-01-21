e252d5d19d4d6cc6c0e33a634f7a695b558ed7fd
Jaritsa Cavalcante <1turmashow@gmail.com>
2023-01-21 19:22:09 -0300

    new commit

--- script.js
+++ script.js
@@ -1,14 +1,31 @@
 const form = document.querySelector('#form-habits')
 const nlwSetup = new NLWSetup(form)
+const button = document.querySelector("header button")
 
-const data = {
-  run: ["01-01", "03-01", "01-06", "01-08", "01-09","01-10"],
-  water: ["01-01", "01-02", "01-03","01-08", "01-11","01-12"],
-  food: ["01-06", "01-07", "01-08","01-09", "01-19","01-20"],
-  read: ["01-03", "01-05", "01-06","01-10", "01-15","01-12"],
-  gym: ["02-02", "02-02", "02-06","02-08"],
 
+button.addEventListener("click", add)
+form.addEventListener("change", save)
+
+function add() {
+  const today = new Date().toLocaleDateString('pt-br').slice(0,-5)
+  const dayExists = nlwSetup.dayExists(today)
+
+  if (dayExists) {
+    alert("Dia já incluso\u{1F4F6}")
+    return
+  }
+
+  alert("Adicionado com sucesso\u{2705}")
+  nlwSetup.addDay(today)
 }
 
+function save() {
+  localStorage.setItem('NLWSetup@habits', JSON.stringify(nlwSetup.data)) 
+}
+
+const data = JSON.parse(localStorage.getItem("NLWSetup@habits")) || {} //se usar em outro navegador 
+
 nlwSetup.setData(data)
-nlwSetup.load() 
+nlwSetup.load()
+
+
